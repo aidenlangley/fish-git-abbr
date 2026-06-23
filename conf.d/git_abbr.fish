3837557a1dae26434f583ae303a88b7a11e82fd7
Aiden Langley <aiden@nedia.dev>
2026-06-24 01:51:06 +1200

    chore: running `:sort` to sort each code block alphabetically

--- conf.d/git_abbr.fish
+++ conf.d/git_abbr.fish
@@ -7,15 +7,16 @@ abbr -c git aa 'add --all'
 abbr -c git apa 'add --patch'
 abbr -c git au 'add --update'
 abbr -c git av 'add --verbose'
+
 abbr -c git ap apply
 abbr -c git apt 'apply --3way'
 
 abbr -c git b branch
+abbr -c git bD 'branch -D'
+abbr -c git bDf 'branch -D -f'
 abbr -c git ba 'branch -a'
 abbr -c git bd 'branch -d'
 abbr -c git bdf 'branch -d -f'
-abbr -c git bD 'branch -D'
-abbr -c git bDf 'branch -D -f'
 abbr -c git bnm 'branch --no-merged'
 abbr -c git br 'branch --remote'
 
@@ -28,20 +29,20 @@ abbr -c git bsr 'bisect reset'
 abbr -c git bss 'bisect start'
 
 abbr -c git c 'commit -v'
-abbr -c git ci 'commit --allow-empty -v -m\'chore: initial commit\''
 abbr -c git c! 'commit -v --amend'
-abbr -c git cn 'commit -v --no-edit'
-abbr -c git cn! 'commit -v --amend --no-edit'
 abbr -c git ca 'commit -a -v'
 abbr -c git ca! 'commit -a -v --amend'
+abbr -c git cam --set-cursor "commit -a -m '%'"
 abbr -c git can! 'commit -a -v --no-edit --amend'
 abbr -c git cans! 'commit -a -v -s --no-edit --amend'
-abbr -c git cam --set-cursor "commit -a -m '%'"
 abbr -c git cas 'commit -a -s'
 abbr -c git casm --set-cursor "commit -a -s -m '%'"
-abbr -c git csm --set-cursor "commit -s -m '%'"
+abbr -c git ci 'commit --allow-empty -v -m\'chore: initial commit\''
 abbr -c git cm --set-cursor "commit -m '%'"
+abbr -c git cn 'commit -v --no-edit'
+abbr -c git cn! 'commit -v --amend --no-edit'
 abbr -c git cs 'commit -S'
+abbr -c git csm --set-cursor "commit -s -m '%'"
 
 abbr -c git cf 'config --list'
 
@@ -50,15 +51,15 @@ abbr -c git cl 'clone --recurse-submodules'
 abbr -c git clean 'clean -id'
 
 abbr -c git co checkout
-abbr -c git cob --set-cursor 'checkout -b % (git_current_branch) # copy current branch then checkout'
 abbr -c git coB --set-cursor 'checkout -B % (git_current_branch) # copy current branch and reset it, then checkout'
-abbr -c git com 'checkout (git_main_branch)'
+abbr -c git cob --set-cursor 'checkout -b % (git_current_branch) # copy current branch then checkout'
 abbr -c git cod 'checkout (git_develop_branch)'
 abbr -c git cof 'checkout (git_feature_prepend)/'
 abbr -c git coh 'checkout hotfix/'
+abbr -c git com 'checkout (git_main_branch)'
 abbr -c git cor 'checkout release/'
-abbr -c git cos 'checkout support/'
 abbr -c git cors 'checkout --recurse-submodules'
+abbr -c git cos 'checkout support/'
 
 abbr -c git count 'shortlog -sn'
 
@@ -68,11 +69,12 @@ abbr -c git cpc 'cherry-pick --continue'
 
 abbr -c git d diff
 abbr -c git dca 'diff --cached'
-abbr -c git dcw 'diff --cached --word-diff'
 abbr -c git dct 'diff --staged'
+abbr -c git dcw 'diff --cached --word-diff'
 abbr -c git dt 'diff-tree --no-commit-id --name-only -r'
-# abbr -c git dnolock 'diff ":(exclude)package-lock.json" ":(exclude)*.lock"'
 abbr -c git dup 'diff @{upstream}'
+
+# abbr -c git dnolock 'diff ":(exclude)package-lock.json" ":(exclude)*.lock"'
 # abbr -c git dv 'diff -w $@ | view -'
 
 abbr -c git dct 'describe --tags (git rev-list --tags --max-count=1)'
@@ -107,14 +109,14 @@ abbr -c git fg 'ls-files | grep'
 
 # gl: git log
 abbr -c git l log
-abbr -c git ls 'log --stat'
-abbr -c git lsp 'log --stat -p'
 abbr -c git lg 'log --graph'
 abbr -c git lgda 'log --graph --decorate --all'
 abbr -c git lgm 'log --graph --max-count=10'
 abbr -c git lo 'log --oneline --decorate'
 abbr -c git log 'log --oneline --decorate --graph'
 abbr -c git loga 'log --oneline --decorate --graph --all'
+abbr -c git ls 'log --stat'
+abbr -c git lsp 'log --stat -p'
 # abbr -c git lol
 # abbr -c git lols
 # abbr -c git lod
@@ -123,9 +125,9 @@ abbr -c git loga 'log --oneline --decorate --graph --all'
 
 # gm: git merge
 abbr -c git m merge
+abbr -c git ma 'merge --abort'
 abbr -c git mom 'merge origin/(git_main_branch)'
 abbr -c git mum 'merge upstream/(git_main_branch)'
-abbr -c git ma 'merge --abort'
 
 # gmtl: git mergetool
 abbr -c git mtl 'mergetool --no-prompt'
@@ -136,13 +138,13 @@ abbr -c git p push
 abbr -c git pd 'push --dry-run'
 abbr -c git pf 'push --force-with-lease'
 abbr -c git pf! 'push --force'
+abbr -c git poat 'push origin --all && git push origin --tags'
+abbr -c git poatf! 'push origin --all --force && git push origin --tags --force'
+abbr -c git poatf! 'push origin --all --force-with-lease && git push origin --tags --force-with-lease'
 abbr -c git psu 'push --set-upstream origin (git_current_branch)'
 abbr -c git pt 'push --tags'
 abbr -c git ptf 'push --tags --force-with-lease'
 abbr -c git ptf! 'push --tags --force'
-abbr -c git poat 'push origin --all && git push origin --tags'
-abbr -c git poatf! 'push origin --all --force-with-lease && git push origin --tags --force-with-lease'
-abbr -c git poatf! 'push origin --all --force && git push origin --tags --force'
 abbr -c git pv 'push -v'
 
 # gpl: git pull
@@ -156,8 +158,8 @@ abbr -c git plum 'pull upstream (git_main_branch)'
 abbr -c git r 'remote -v'
 abbr -c git ra 'remote add'
 abbr -c git rau 'remote add upstream'
-abbr -c git rrm 'remote remove'
 abbr -c git rmv 'remote rename'
+abbr -c git rrm 'remote remove'
 abbr -c git rset 'remote set-url'
 abbr -c git ru 'remote update'
 abbr -c git rv 'remote -v'
@@ -169,8 +171,8 @@ abbr -c git rba 'rebase --abort'
 abbr -c git rbc 'rebase --continue'
 abbr -c git rbd 'rebase (git_develop_branch)'
 abbr -c git rbi 'rebase -i'
-abbr -c git rbom 'rebase origin/(git_main_branch)'
 abbr -c git rbo 'rebase --onto'
+abbr -c git rbom 'rebase origin/(git_main_branch)'
 abbr -c git rbs 'rebase --skip'
 
 # grev: git revert
@@ -178,15 +180,15 @@ abbr -c git rev revert
 
 # grs: git reset
 abbr -c git rs reset
-abbr -c git rss 'reset --soft'
+abbr -c git pristine 'reset --hard && git clean -dffx'
 abbr -c git rs! 'reset --hard'
+abbr -c git rs- 'reset --'
 abbr -c git rsh 'reset HEAD^'
-abbr -c git rssh 'reset --soft HEAD^'
 abbr -c git rsh! 'reset --hard HEAD^'
 abbr -c git rsoh 'reset origin/(git_current_branch)'
 abbr -c git rsoh! 'reset origin/(git_current_branch) --hard'
-abbr -c git pristine 'reset --hard && git clean -dffx'
-abbr -c git rs- 'reset --'
+abbr -c git rss 'reset --soft'
+abbr -c git rssh 'reset --soft HEAD^'
 
 abbr -c git undo 'reset HEAD^'
 abbr -c git undos 'reset --soft HEAD^'
@@ -206,8 +208,8 @@ abbr -c git rt 'cd (git rev-parse --show-toplevel || echo .)'
 
 # gs: git status
 abbr -c git s status
-abbr -c git ss 'status -s'
 abbr -c git sb 'status -sb'
+abbr -c git ss 'status -s'
 
 # gshow: git show
 abbr -c git show show
@@ -216,14 +218,14 @@ abbr -c git showps 'show --pretty=short --show-signature'
 # gst: git stash
 abbr -c git st stash
 abbr -c git sta 'stash apply'
+abbr -c git stall 'stash --all'
 abbr -c git stc 'stash clear'
 abbr -c git std 'stash drop'
 abbr -c git stl 'stash list'
 abbr -c git stshow 'stash show --text'
-abbr -c git stall 'stash --all'
 
-abbr -a git_stash_push -r 'stp|stu' -c git --set-cursor "stash push -m '%'"
 abbr -a git_stash_pop -r 'stP|sto' -c git 'stash pop'
+abbr -a git_stash_push -r 'stp|stu' -c git --set-cursor "stash push -m '%'"
 
 # gsu: git submodule
 abbr -c git su 'submodule update'
@@ -231,14 +233,14 @@ abbr -c git su 'submodule update'
 # gsw: git switch
 abbr -c git sw switch
 abbr -c git swc 'switch -c'
-abbr -c git swm 'switch (git_main_branch)'
 abbr -c git swd 'switch (git_develop_branch)'
+abbr -c git swm 'switch (git_main_branch)'
 
 # gt: git tag
 abbr -c git t tag
-abbr -c git ts 'tag -s'
 abbr -c git ta 'tag -a'
 abbr -c git tas 'tag -a -s'
+abbr -c git ts 'tag -s'
 # gtl
 
 # gwch: git whatchanged
@@ -253,13 +255,12 @@ abbr -c git wtrm 'worktree remove'
 
 # gam: git am
 abbr -c git am am
+abbr -c git ama 'am --abort'
 abbr -c git amc 'am --continue'
 abbr -c git ams 'am --skip'
-abbr -c git ama 'am --abort'
 abbr -c git amscp 'am --show-current-patch'
 
 function git_abbr_uninstall --on-event git_abbr_uninstall
-
     set -e __git_abbr_version
 
     functions -e git_current_branch
