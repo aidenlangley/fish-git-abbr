5c3788ad8d1656c78a741d82482f97fcf25a878a
Aiden Langley <aiden@nedia.dev>
2026-06-24 00:46:25 +1200

    Instead of `gca`, it's now `g` -> `git`, then `git ca` -> `git commit -a -v`

--- README.md
+++ README.md
@@ -9,183 +9,19 @@ Mainly based off those of [`oh-my-zsh`](https://github.com/robbyrussell/oh-my-zs
 Using [`fisher`](https://github.com/jorgebucaran/fisher):
 
 ```fish
-fisher install lewisacidic/fish-git-abbr
+fisher install aidenlangley/fish-git-abbr
 ```
 
 ## Usage
 
-Fish abbreviations replace typed text with an extended command after pressing \<Space> or \<Enter>.
+Fish abbreviations replace typed text with an extended command after pressing
+\<Space> or \<Enter>.
 
-abbreviation | result
--------------|--------
-`g` | `git`
-`ga` | `git add`
-`gaa` | `git add --all`
-`gapa` | `git add --patch`
-`gau` | `git add --update`
-`gav` | `git add --verbose`
-`gap` | `git apply`
-`gapt` | `git apply --3way`
-`gb` | `git branch`
-`gba` | `git branch -a`
-`gbd` | `git branch -d`
-`gbdf` | `git branch -d -f`
-`gbD` | `git branch -D`
-`gbDf` | `git branch -D -f`
-`gbnm` | `git branch --no-merged`
-`gbr` | `git branch --remote`
-`gbl` | `git blame -b -w`
-`gbs` | `git bisect`
-`gbsb` | `git bisect bad`
-`gbsg` | `git bisect good`
-`gbsr` | `git bisect reset`
-`gbss` | `git bisect start`
-`gc` | `git commit -v`
-`gci` | `git commit -v --allow-empty -m 'chore: initial commit'`
-`gc!` | `git commit -v --amend`
-`gcn` | `git commit -v --no-edit`
-`gcn!` | `git commit -v --amend --no-edit`
-`gca` | `git commit -a -v`
-`gca!` | `git commit -a -v --amend`
-`gcan!` | `git commit -a -v --no-edit --amend`
-`gcans!` | `git commit -a -v -s --no-edit --amend`
-`gcam` | `git commit -a -m`
-`gcas` | `git commit -a -s`
-`gcasm` | `git commit -a -s -m`
-`gcsm` | `git commit -s -m`
-`gcm` | `git commit -m`
-`gcs` | `git commit -S`
-`gcf` | `git config --list`
-`gcl` | `git clone --recurse-submodules`
-`gclean` | `git clean -id`
-`gco` | `git checkout`
-`gcob` | `git checkout -b`
-`gcom` | `git checkout (git_main_branch)`
-`gcod` | `git checkout (git_develop_branch)`
-`gcof` | `git checkout (git_feature_prepend)/`
-`gcoh` | `git checkout hotfix/`
-`gcor` | `git checkout release/`
-`gcos` | `git checkout support/`
-`gcors` | `git checkout --recurse-submodules`
-`gcount` | `git shortlog -sn`
-`gcp` | `git cherry-pick`
-`gcpa` | `git cherry-pick --abort`
-`gcpc` | `git cherry-pick --continue`
-`gd` | `git diff`
-`gdca` | `git diff --cached`
-`gdcw` | `git diff --cached --word-diff`
-`gdct` | `git diff --staged`
-`gdt` | `git diff-tree --no-commit-id --name-only -r`
-`gdup` | `git diff @{upstream}`
-`gdct` | `git describe --tags (git rev-list --tags --max-count=1)`
-`gf` | `git fetch`
-`gfa` | `git fetch --all --prune`
-`gfo` | `git fetch origin`
-`ghh` | `git help`
-`gi` | `git init`
-`gignore` | `git update-index --assume-unchanged`
-`gignored` | `git ls-files -v | grep "^[[:lower:]]"`
-`gk` | `gitk --all --branches &!`
-`gke` | `gitk --all (git log -g --pretty=%h) &!`
-`gfg` | `git ls-files | grep`
-`gl` | `git log`
-`gls` | `git log --stat`
-`glsp` | `git log --stat -p`
-`glg` | `git log --graph`
-`glgda` | `git log --graph --decorate --all`
-`glgm` | `git log --graph --max-count=10`
-`glo` | `git log --oneline --decorate`
-`glog` | `git log --oneline --decorate --graph`
-`gloga` | `git log --oneline --decorate --graph --all`
-`gm` | `git merge`
-`gmom` | `git merge origin/(git_main_branch)`
-`gmum` | `git merge upstream/(git_main_branch)`
-`gma` | `git merge --abort`
-`gmtl` | `git mergetool --no-prompt`
-`gmtlvim` | `git mergetool --no-prompt --tool=vimdiff`
-`gp` | `git push`
-`gpd` | `git push --dry-run`
-`gpf` | `git push --force-with-lease`
-`gpf!` | `git push --force`
-`gpsu` | `git push --set-upstream origin (git_current_branch)`
-`gpt` | `git push --tags`
-`gptf` | `git push --tags --force-with-lease`
-`gptf!` | `git push --tags --force`
-`gpoat` | `git push origin --all && git push origin --tags`
-`gpoatf!` | `git push origin --all --force-with-lease && git push origin --tags --force-with-lease`
-`gpoatf!` | `git push origin --all --force && git push origin --tags --force`
-`gpv` | `git push -v`
-`gpl` | `git pull`
-`gplo` | `git pull origin`
-`gplom` | `git pull origin (git_main_branch)`
-`gplu` | `git pull upstream`
-`gplum` | `git pull upstream (git_main_branch)`
-`gr` | `git remote -v`
-`gra` | `git remote add`
-`grau` | `git remote add upstream`
-`grrm` | `git remote remove`
-`grmv` | `git remote rename`
-`grset` | `git remote set-url`
-`gru` | `git remote update`
-`grv` | `git remote -v`
-`grvv` | `git remote -vvv`
-`grb` | `git rebase`
-`grba` | `git rebase --abort`
-`grbc` | `git rebase --continue`
-`grbd` | `git rebase (git_develop_branch)`
-`grbi` | `git rebase -i`
-`grbom` | `git rebase origin/(git_main_branch)`
-`grbo` | `git rebase --onto`
-`grbs` | `git rebase --skip`
-`grev` | `git revert`
-`grs` | `git reset`
-`grs!` | `git reset --hard`
-`grsh` | `git reset HEAD`
-`grsh!` | `git reset HEAD --hard`
-`grsoh` | `git reset origin/(git_current_branch)`
-`grsoh!` | `git reset origin/(git_current_branch) --hard`
-`gpristine` | `git reset --hard && git clean -dffx`
-`grs-` | `git reset --`
-`grm` | `git rm`
-`grmc` | `git rm --cached`
-`grst` | `git restore`
-`grsts` | `git restore --source`
-`grstst` | `git restore --staged`
-`grt` | `cd (git rev-parse --show-toplevel || echo .)`
-`gs` | `git status`
-`gss` | `git status -s`
-`gsb` | `git status -sb`
-`gshow` | `git show`
-`gshowps` | `git show --pretty=short --show-signature`
-`gst` | `git stash`
-`gsta` | `git stash apply`
-`gstc` | `git stash clear`
-`gstd` | `git stash drop`
-`gstl` | `git stash list`
-`gstp` | `git stash pop`
-`gstshow` | `git stash show --text`
-`gstall` | `git stash --all`
-`gsts` | `git stash save`
-`gsu` | `git submodule update`
-`gsw` | `git switch`
-`gswc` | `git switch -c`
-`gswm` | `git switch (git_main_branch)`
-`gswd` | `git switch (git_develop_branch)`
-`gt` | `git tag`
-`gts` | `git tag -s`
-`gta` | `git tag -a`
-`gtas` | `git tag -a -s`
-`gwch` | `git whatchanged -p --abbrev-commit --pretty=medium`
-`gwt` | `git worktree`
-`gwta` | `git worktree add`
-`gwtls` | `git worktree list`
-`gwtmv` | `git worktree move`
-`gwtrm` | `git worktree remove`
-`gam` | `git am`
-`gamc` | `git am --continue`
-`gams` | `git am --skip`
-`gama` | `git am --abort`
-`gamscp` | `git am --show-current-patch`
+**See [upstream](https://github.com/lewisacidic/fish-git-abbr) for full list of abbreviations.**
+
+I have changed the abbreviations so `g` triggers `git`, which can then be
+followed by the remaining characters, such as `cam` for `commit -a -m`. I have
+also used `--set-cursor` to move the cursor inside quotes for the message string.
 
 ## See Also
 
--- conf.d/git_abbr.fish
+++ conf.d/git_abbr.fish
@@ -1,91 +1,84 @@
-#!/usr/bin/env fish
-#
-# git-abbr: git abbreviations for the fish shell
-#
-# Copyright (c) 2022 Rich Lewis
-# MIT License
+set -g __git_abbr_version 0.3.0
 
-set -g __git_abbr_version 0.2.1
+abbr g git
 
-abbr g 'git'
+abbr -c git a add
+abbr -c git aa 'add --all'
+abbr -c git apa 'add --patch'
+abbr -c git au 'add --update'
+abbr -c git av 'add --verbose'
+abbr -c git ap apply
+abbr -c git apt 'apply --3way'
 
-abbr ga 'git add'
-abbr gaa 'git add --all'
-abbr gapa 'git add --patch'
-abbr gau 'git add --update'
-abbr gav 'git add --verbose'
-abbr gap 'git apply'
-abbr gapt 'git apply --3way'
+abbr -c git b branch
+abbr -c git ba 'branch -a'
+abbr -c git bd 'branch -d'
+abbr -c git bdf 'branch -d -f'
+abbr -c git bD 'branch -D'
+abbr -c git bDf 'branch -D -f'
+abbr -c git bnm 'branch --no-merged'
+abbr -c git br 'branch --remote'
 
-abbr gb 'git branch'
-abbr gba 'git branch -a'
-abbr gbd 'git branch -d'
-abbr gbdf 'git branch -d -f'
-abbr gbD 'git branch -D'
-abbr gbDf 'git branch -D -f'
-abbr gbnm 'git branch --no-merged'
-abbr gbr 'git branch --remote'
+abbr -c git bl 'blame -b -w'
 
-abbr gbl 'git blame -b -w'
+abbr -c git bs bisect
+abbr -c git bsb 'bisect bad'
+abbr -c git bsg 'bisect good'
+abbr -c git bsr 'bisect reset'
+abbr -c git bss 'bisect start'
 
-abbr gbs 'git bisect'
-abbr gbsb 'git bisect bad'
-abbr gbsg 'git bisect good'
-abbr gbsr 'git bisect reset'
-abbr gbss 'git bisect start'
+abbr -c git c 'commit -v'
+abbr -c git ci 'commit --allow-empty -v -m\'chore: initial commit\''
+abbr -c git c! 'commit -v --amend'
+abbr -c git cn 'commit -v --no-edit'
+abbr -c git cn! 'commit -v --amend --no-edit'
+abbr -c git ca 'commit -a -v'
+abbr -c git ca! 'commit -a -v --amend'
+abbr -c git can! 'commit -a -v --no-edit --amend'
+abbr -c git cans! 'commit -a -v -s --no-edit --amend'
+abbr -c git cam 'commit -a -m'
+abbr -c git cas 'commit -a -s'
+abbr -c git casm 'commit -a -s -m'
+abbr -c git csm 'commit -s -m'
+abbr -c git cm 'commit -m'
+abbr -c git cs 'commit -S'
 
-abbr gc 'git commit -v'
-abbr gci 'git commit --allow-empty -v -m\'chore: initial commit\''
-abbr gc! 'git commit -v --amend'
-abbr gcn 'git commit -v --no-edit'
-abbr gcn! 'git commit -v --amend --no-edit'
-abbr gca 'git commit -a -v'
-abbr gca! 'git commit -a -v --amend'
-abbr gcan! 'git commit -a -v --no-edit --amend'
-abbr gcans! 'git commit -a -v -s --no-edit --amend'
-abbr gcam 'git commit -a -m'
-abbr gcas 'git commit -a -s'
-abbr gcasm 'git commit -a -s -m'
-abbr gcsm 'git commit -s -m'
-abbr gcm 'git commit -m'
-abbr gcs 'git commit -S'
+abbr -c git cf 'config --list'
 
-abbr gcf 'git config --list'
+abbr -c git cl 'clone --recurse-submodules'
 
-abbr gcl 'git clone --recurse-submodules'
+abbr -c git clean 'clean -id'
 
-abbr gclean 'git clean -id'
+abbr -c git co checkout
+abbr -c git cob 'checkout -b'
+abbr -c git com 'checkout (git_main_branch)'
+abbr -c git cod 'checkout (git_develop_branch)'
+abbr -c git cof 'checkout (git_feature_prepend)/'
+abbr -c git coh 'checkout hotfix/'
+abbr -c git cor 'checkout release/'
+abbr -c git cos 'checkout support/'
+abbr -c git cors 'checkout --recurse-submodules'
 
-abbr gco 'git checkout'
-abbr gcob 'git checkout -b'
-abbr gcom 'git checkout (git_main_branch)'
-abbr gcod 'git checkout (git_develop_branch)'
-abbr gcof 'git checkout (git_feature_prepend)/'
-abbr gcoh 'git checkout hotfix/'
-abbr gcor 'git checkout release/'
-abbr gcos 'git checkout support/'
-abbr gcors 'git checkout --recurse-submodules'
+abbr -c git count 'shortlog -sn'
 
-abbr gcount 'git shortlog -sn'
+abbr -c git cp cherry-pick
+abbr -c git cpa 'cherry-pick --abort'
+abbr -c git cpc 'cherry-pick --continue'
 
-abbr gcp 'git cherry-pick'
-abbr gcpa 'git cherry-pick --abort'
-abbr gcpc 'git cherry-pick --continue'
+abbr -c git d diff
+abbr -c git dca 'diff --cached'
+abbr -c git dcw 'diff --cached --word-diff'
+abbr -c git dct 'diff --staged'
+abbr -c git dt 'diff-tree --no-commit-id --name-only -r'
+# abbr -c git dnolock 'diff ":(exclude)package-lock.json" ":(exclude)*.lock"'
+abbr -c git dup 'diff @{upstream}'
+# abbr -c git dv 'diff -w $@ | view -'
 
-abbr gd 'git diff'
-abbr gdca 'git diff --cached'
-abbr gdcw 'git diff --cached --word-diff'
-abbr gdct 'git diff --staged'
-abbr gdt 'git diff-tree --no-commit-id --name-only -r'
-# abbr gdnolock 'git diff ":(exclude)package-lock.json" ":(exclude)*.lock"'
-abbr gdup 'git diff @{upstream}'
-# abbr gdv 'git diff -w $@ | view -'
+abbr -c git dct 'describe --tags (git rev-list --tags --max-count=1)'
 
-abbr gdct 'git describe --tags (git rev-list --tags --max-count=1)'
-
-abbr gf 'git fetch'
-abbr gfa 'git fetch --all --prune'
-abbr gfo 'git fetch origin'
+abbr -c git f fetch
+abbr -c git fa 'fetch --all --prune'
+abbr -c git fo 'fetch origin'
 
 # gg
 # gga
@@ -101,338 +94,338 @@ abbr gfo 'git fetch origin'
 # ggu
 # gpsup
 
-abbr ghh 'git help'
+abbr -c git hh help
 
-abbr gi 'git init'
+abbr -c git i init
 
-abbr gignore 'git update-index --assume-unchanged'
-abbr gignored 'git ls-files -v | grep "^[[:lower:]]"'
+abbr -c git ignore 'update-index --assume-unchanged'
+abbr -c git ignored 'ls-files -v | grep "^[[:lower:]]"'
 
-abbr gk 'gitk --all --branches &!'
-abbr gke 'gitk --all (git log -g --pretty=%h) &!'
+abbr -c git k 'gitk --all --branches &!'
+abbr -c git ke 'gitk --all (git log -g --pretty=%h) &!'
 
-abbr gfg 'git ls-files | grep'
+abbr -c git fg 'ls-files | grep'
 
 # gl: git log
-abbr gl 'git log'
-abbr gls 'git log --stat'
-abbr glsp 'git log --stat -p'
-abbr glg 'git log --graph'
-abbr glgda 'git log --graph --decorate --all'
-abbr glgm 'git log --graph --max-count=10'
-abbr glo 'git log --oneline --decorate'
-abbr glog 'git log --oneline --decorate --graph'
-abbr gloga 'git log --oneline --decorate --graph --all'
-# abbr glol
-# abbr glols
-# abbr glod
-# abbr glods
-# abbr glola
+abbr -c git l log
+abbr -c git ls 'log --stat'
+abbr -c git lsp 'log --stat -p'
+abbr -c git lg 'log --graph'
+abbr -c git lgda 'log --graph --decorate --all'
+abbr -c git lgm 'log --graph --max-count=10'
+abbr -c git lo 'log --oneline --decorate'
+abbr -c git log 'log --oneline --decorate --graph'
+abbr -c git loga 'log --oneline --decorate --graph --all'
+# abbr -c git lol
+# abbr -c git lols
+# abbr -c git lod
+# abbr -c git lods
+# abbr -c git lola
 
 # gm: git merge
-abbr gm 'git merge'
-abbr gmom 'git merge origin/(git_main_branch)'
-abbr gmum 'git merge upstream/(git_main_branch)'
-abbr gma 'git merge --abort'
+abbr -c git m merge
+abbr -c git mom 'merge origin/(git_main_branch)'
+abbr -c git mum 'merge upstream/(git_main_branch)'
+abbr -c git ma 'merge --abort'
 
 # gmtl: git mergetool
-abbr gmtl 'git mergetool --no-prompt'
-abbr gmtlvim 'git mergetool --no-prompt --tool=vimdiff'
+abbr -c git mtl 'mergetool --no-prompt'
+abbr -c git mtlvim 'mergetool --no-prompt --tool=vimdiff'
 
 # gp: git push
-abbr gp 'git push'
-abbr gpd 'git push --dry-run'
-abbr gpf 'git push --force-with-lease'
-abbr gpf! 'git push --force'
-abbr gpsu 'git push --set-upstream origin (git_current_branch)'
-abbr gpt 'git push --tags'
-abbr gptf 'git push --tags --force-with-lease'
-abbr gptf! 'git push --tags --force'
-abbr gpoat 'git push origin --all && git push origin --tags'
-abbr gpoatf! 'git push origin --all --force-with-lease && git push origin --tags --force-with-lease'
-abbr gpoatf! 'git push origin --all --force && git push origin --tags --force'
-abbr gpv 'git push -v'
+abbr -c git p push
+abbr -c git pd 'push --dry-run'
+abbr -c git pf 'push --force-with-lease'
+abbr -c git pf! 'push --force'
+abbr -c git psu 'push --set-upstream origin (git_current_branch)'
+abbr -c git pt 'push --tags'
+abbr -c git ptf 'push --tags --force-with-lease'
+abbr -c git ptf! 'push --tags --force'
+abbr -c git poat 'push origin --all && git push origin --tags'
+abbr -c git poatf! 'push origin --all --force-with-lease && git push origin --tags --force-with-lease'
+abbr -c git poatf! 'push origin --all --force && git push origin --tags --force'
+abbr -c git pv 'push -v'
 
 # gpl: git pull
-abbr gpl 'git pull'
-abbr gplo 'git pull origin'
-abbr gplom 'git pull origin (git_main_branch)'
-abbr gplu 'git pull upstream'
-abbr gplum 'git pull upstream (git_main_branch)'
+abbr -c git pl pull
+abbr -c git plo 'pull origin'
+abbr -c git plom 'pull origin (git_main_branch)'
+abbr -c git plu 'pull upstream'
+abbr -c git plum 'pull upstream (git_main_branch)'
 
 # gr: git remote
-abbr gr 'git remote -v'
-abbr gra 'git remote add'
-abbr grau 'git remote add upstream'
-abbr grrm 'git remote remove'
-abbr grmv 'git remote rename'
-abbr grset 'git remote set-url'
-abbr gru 'git remote update'
-abbr grv 'git remote -v'
-abbr grvv 'git remote -vvv'
+abbr -c git r 'remote -v'
+abbr -c git ra 'remote add'
+abbr -c git rau 'remote add upstream'
+abbr -c git rrm 'remote remove'
+abbr -c git rmv 'remote rename'
+abbr -c git rset 'remote set-url'
+abbr -c git ru 'remote update'
+abbr -c git rv 'remote -v'
+abbr -c git rvv 'remote -vvv'
 
 # grb: git rebase
-abbr grb 'git rebase'
-abbr grba 'git rebase --abort'
-abbr grbc 'git rebase --continue'
-abbr grbd 'git rebase (git_develop_branch)'
-abbr grbi 'git rebase -i'
-abbr grbom 'git rebase origin/(git_main_branch)'
-abbr grbo 'git rebase --onto'
-abbr grbs 'git rebase --skip'
+abbr -c git rb rebase
+abbr -c git rba 'rebase --abort'
+abbr -c git rbc 'rebase --continue'
+abbr -c git rbd 'rebase (git_develop_branch)'
+abbr -c git rbi 'rebase -i'
+abbr -c git rbom 'rebase origin/(git_main_branch)'
+abbr -c git rbo 'rebase --onto'
+abbr -c git rbs 'rebase --skip'
 
 # grev: git revert
-abbr grev 'git revert'
+abbr -c git rev revert
 
 # grs: git reset
-abbr grs 'git reset'
-abbr grs! 'git reset --hard'
-abbr grsh 'git reset HEAD'
-abbr grsh! 'git reset HEAD --hard'
-abbr grsoh 'git reset origin/(git_current_branch)'
-abbr grsoh! 'git reset origin/(git_current_branch) --hard'
-abbr gpristine 'git reset --hard && git clean -dffx'
-abbr grs- 'git reset --'
+abbr -c git rs reset
+abbr -c git rs! 'reset --hard'
+abbr -c git rsh 'reset HEAD'
+abbr -c git rsh! 'reset HEAD --hard'
+abbr -c git rsoh 'reset origin/(git_current_branch)'
+abbr -c git rsoh! 'reset origin/(git_current_branch) --hard'
+abbr -c git pristine 'reset --hard && git clean -dffx'
+abbr -c git rs- 'reset --'
 
 # grm: git rm
-abbr grm 'git rm'
-abbr grmc 'git rm --cached'
+abbr -c git rm rm
+abbr -c git rmc 'rm --cached'
 
 # grst: git restore
-abbr grst 'git restore'
-abbr grsts 'git restore --source'
-abbr grstst 'git restore --staged'
+abbr -c git rst restore
+abbr -c git rsts 'restore --source'
+abbr -c git rstst 'restore --staged'
 
 # grt: git return
-abbr grt 'cd (git rev-parse --show-toplevel || echo .)'
+abbr -c git rt 'cd (git rev-parse --show-toplevel || echo .)'
 
 # gs: git status
-abbr gs 'git status'
-abbr gss 'git status -s'
-abbr gsb 'git status -sb'
+abbr -c git s status
+abbr -c git ss 'status -s'
+abbr -c git sb 'status -sb'
 
 # gshow: git show
-abbr gshow 'git show'
-abbr gshowps 'git show --pretty=short --show-signature'
+abbr -c git show show
+abbr -c git showps 'show --pretty=short --show-signature'
 
 # gst: git stash
-abbr gst 'git stash'
-abbr gsta 'git stash apply'
-abbr gstc 'git stash clear'
-abbr gstd 'git stash drop'
-abbr gstl 'git stash list'
-abbr gstp 'git stash pop'
-abbr gstshow 'git stash show --text'
-abbr gstall 'git stash --all'
-abbr gsts 'git stash save'
+abbr -c git st stash
+abbr -c git sta 'stash apply'
+abbr -c git stc 'stash clear'
+abbr -c git std 'stash drop'
+abbr -c git stl 'stash list'
+abbr -c git stp 'stash pop'
+abbr -c git stshow 'stash show --text'
+abbr -c git stall 'stash --all'
+abbr -c git sts 'stash save'
 
 # gsu: git submodule
-abbr gsu 'git submodule update'
+abbr -c git su 'submodule update'
 
 # gsw: git switch
-abbr gsw 'git switch'
-abbr gswc 'git switch -c'
-abbr gswm 'git switch (git_main_branch)'
-abbr gswd 'git switch (git_develop_branch)'
+abbr -c git sw switch
+abbr -c git swc 'switch -c'
+abbr -c git swm 'switch (git_main_branch)'
+abbr -c git swd 'switch (git_develop_branch)'
 
 # gt: git tag
-abbr gt 'git tag'
-abbr gts 'git tag -s'
-abbr gta 'git tag -a'
-abbr gtas 'git tag -a -s'
+abbr -c git t tag
+abbr -c git ts 'tag -s'
+abbr -c git ta 'tag -a'
+abbr -c git tas 'tag -a -s'
 # gtl
 
 # gwch: git whatchanged
-abbr gwch 'git whatchanged -p --abbrev-commit --pretty=medium'
+abbr -c git wch 'whatchanged -p --abbrev-commit --pretty=medium'
 
 # gwt: git worktree
-abbr gwt 'git worktree'
-abbr gwta 'git worktree add'
-abbr gwtls 'git worktree list'
-abbr gwtmv 'git worktree move'
-abbr gwtrm 'git worktree remove'
+abbr -c git wt worktree
+abbr -c git wta 'worktree add'
+abbr -c git wtls 'worktree list'
+abbr -c git wtmv 'worktree move'
+abbr -c git wtrm 'worktree remove'
 
 # gam: git am
-abbr gam 'git am'
-abbr gamc 'git am --continue'
-abbr gams 'git am --skip'
-abbr gama 'git am --abort'
-abbr gamscp 'git am --show-current-patch'
+abbr -c git am am
+abbr -c git amc 'am --continue'
+abbr -c git ams 'am --skip'
+abbr -c git ama 'am --abort'
+abbr -c git amscp 'am --show-current-patch'
 
 function git_abbr_uninstall --on-event git_abbr_uninstall
 
-  set -e __git_abbr_version
+    set -e __git_abbr_version
 
-  functions -e git_current_branch
-  functions -e git_main_branch
-  functions -e git_develop_branch
-  functions -e git_feature_branch_prepend
+    functions -e git_current_branch
+    functions -e git_main_branch
+    functions -e git_develop_branch
+    functions -e git_feature_branch_prepend
 
-  abbr -e g
-  abbr -e ga
-  abbr -e gaa
-  abbr -e gapa
-  abbr -e gau
-  abbr -e gav
-  abbr -e gap
-  abbr -e gapt
-  abbr -e gb
-  abbr -e gba
-  abbr -e gbd
-  abbr -e gbDf
-  abbr -e gbD
-  abbr -e gbDf
-  abbr -e gbnm
-  abbr -e gbr
-  abbr -e gbl
-  abbr -e gbs
-  abbr -e gbsb
-  abbr -e gbsg
-  abbr -e gbsr
-  abbr -e gbss
-  abbr -e gc
-  abbr -e gci
-  abbr -e gc!
-  abbr -e gcn
-  abbr -e gcn!
-  abbr -e gca
-  abbr -e gca!
-  abbr -e gcan!
-  abbr -e gcans!
-  abbr -e gcam
-  abbr -e gcas
-  abbr -e gcasm
-  abbr -e gcsm
-  abbr -e gcm
-  abbr -e gcs
-  abbr -e gcf
-  abbr -e gcl
-  abbr -e gclean
-  abbr -e gco
-  abbr -e gcob
-  abbr -e gcom
-  abbr -e gcod
-  abbr -e gcof
-  abbr -e gcoh
-  abbr -e gcor
-  abbr -e gcos
-  abbr -e gcors
-  abbr -e gcount
-  abbr -e gcp
-  abbr -e gcpa
-  abbr -e gcpc
-  abbr -e gd
-  abbr -e gdca
-  abbr -e gdcw
-  abbr -e gdct
-  abbr -e gdt
-  abbr -e gdup
-  abbr -e gdct
-  abbr -e gf
-  abbr -e gfa
-  abbr -e gfo
-  abbr -e ghh
-  abbr -e gi
-  abbr -e gignore
-  abbr -e gignored
-  abbr -e gk
-  abbr -e gke
-  abbr -e gfg
-  abbr -e gl
-  abbr -e gls
-  abbr -e glsp
-  abbr -e glg
-  abbr -e glgda
-  abbr -e glgm
-  abbr -e glo
-  abbr -e glog
-  abbr -e gloga
-  abbr -e gm
-  abbr -e gmom
-  abbr -e gmum
-  abbr -e gma
-  abbr -e gmtl
-  abbr -e gmtlvim
-  abbr -e gp
-  abbr -e gpd
-  abbr -e gpf
-  abbr -e gpf!
-  abbr -e gpt
-  abbr -e gptf
-  abbr -e gptf!
-  abbr -e gpoat
-  abbr -e gpoatf!
-  abbr -e gpoatf!
-  abbr -e gpv
-  abbr -e gpl
-  abbr -e gplo
-  abbr -e gplom
-  abbr -e gplu
-  abbr -e gplum
-  abbr -e gr
-  abbr -e gra
-  abbr -e grau
-  abbr -e grrm
-  abbr -e grmv
-  abbr -e grset
-  abbr -e gru
-  abbr -e grv
-  abbr -e grvv
-  abbr -e grb
-  abbr -e grba
-  abbr -e grbc
-  abbr -e grbd
-  abbr -e grbi
-  abbr -e grbom
-  abbr -e grbo
-  abbr -e grbs
-  abbr -e grev
-  abbr -e grs
-  abbr -e grs!
-  abbr -e grsh
-  abbr -e grsh!
-  abbr -e grsoh
-  abbr -e grsoh!
-  abbr -e gpristine
-  abbr -e grs-
-  abbr -e grm
-  abbr -e grmc
-  abbr -e grst
-  abbr -e grsts
-  abbr -e grstst
-  abbr -e grt
-  abbr -e gs
-  abbr -e gss
-  abbr -e gsb
-  abbr -e gshow
-  abbr -e gshowps
-  abbr -e gst
-  abbr -e gsta
-  abbr -e gstc
-  abbr -e gstd
-  abbr -e gstl
-  abbr -e gstp
-  abbr -e gstshow
-  abbr -e gstall
-  abbr -e gsts
-  abbr -e gsu
-  abbr -e gsw
-  abbr -e gswc
-  abbr -e gswm
-  abbr -e gswd
-  abbr -e gt
-  abbr -e gts
-  abbr -e gta
-  abbr -e gtas
-  abbr -e gwch
-  abbr -e gwt
-  abbr -e gwta
-  abbr -e gwtls
-  abbr -e gwtmv
-  abbr -e gwtrm
-  abbr -e gam
-  abbr -e gamc
-  abbr -e gams
-  abbr -e gama
-  abbr -e gamscp
+    abbr -e g
+    abbr -e ga
+    abbr -e gaa
+    abbr -e gapa
+    abbr -e gau
+    abbr -e gav
+    abbr -e gap
+    abbr -e gapt
+    abbr -e gb
+    abbr -e gba
+    abbr -e gbd
+    abbr -e gbDf
+    abbr -e gbD
+    abbr -e gbDf
+    abbr -e gbnm
+    abbr -e gbr
+    abbr -e gbl
+    abbr -e gbs
+    abbr -e gbsb
+    abbr -e gbsg
+    abbr -e gbsr
+    abbr -e gbss
+    abbr -e gc
+    abbr -e gci
+    abbr -e gc!
+    abbr -e gcn
+    abbr -e gcn!
+    abbr -e gca
+    abbr -e gca!
+    abbr -e gcan!
+    abbr -e gcans!
+    abbr -e gcam
+    abbr -e gcas
+    abbr -e gcasm
+    abbr -e gcsm
+    abbr -e gcm
+    abbr -e gcs
+    abbr -e gcf
+    abbr -e gcl
+    abbr -e gclean
+    abbr -e gco
+    abbr -e gcob
+    abbr -e gcom
+    abbr -e gcod
+    abbr -e gcof
+    abbr -e gcoh
+    abbr -e gcor
+    abbr -e gcos
+    abbr -e gcors
+    abbr -e gcount
+    abbr -e gcp
+    abbr -e gcpa
+    abbr -e gcpc
+    abbr -e gd
+    abbr -e gdca
+    abbr -e gdcw
+    abbr -e gdct
+    abbr -e gdt
+    abbr -e gdup
+    abbr -e gdct
+    abbr -e gf
+    abbr -e gfa
+    abbr -e gfo
+    abbr -e ghh
+    abbr -e gi
+    abbr -e gignore
+    abbr -e gignored
+    abbr -e gk
+    abbr -e gke
+    abbr -e gfg
+    abbr -e gl
+    abbr -e gls
+    abbr -e glsp
+    abbr -e glg
+    abbr -e glgda
+    abbr -e glgm
+    abbr -e glo
+    abbr -e glog
+    abbr -e gloga
+    abbr -e gm
+    abbr -e gmom
+    abbr -e gmum
+    abbr -e gma
+    abbr -e gmtl
+    abbr -e gmtlvim
+    abbr -e gp
+    abbr -e gpd
+    abbr -e gpf
+    abbr -e gpf!
+    abbr -e gpt
+    abbr -e gptf
+    abbr -e gptf!
+    abbr -e gpoat
+    abbr -e gpoatf!
+    abbr -e gpoatf!
+    abbr -e gpv
+    abbr -e gpl
+    abbr -e gplo
+    abbr -e gplom
+    abbr -e gplu
+    abbr -e gplum
+    abbr -e gr
+    abbr -e gra
+    abbr -e grau
+    abbr -e grrm
+    abbr -e grmv
+    abbr -e grset
+    abbr -e gru
+    abbr -e grv
+    abbr -e grvv
+    abbr -e grb
+    abbr -e grba
+    abbr -e grbc
+    abbr -e grbd
+    abbr -e grbi
+    abbr -e grbom
+    abbr -e grbo
+    abbr -e grbs
+    abbr -e grev
+    abbr -e grs
+    abbr -e grs!
+    abbr -e grsh
+    abbr -e grsh!
+    abbr -e grsoh
+    abbr -e grsoh!
+    abbr -e gpristine
+    abbr -e grs-
+    abbr -e grm
+    abbr -e grmc
+    abbr -e grst
+    abbr -e grsts
+    abbr -e grstst
+    abbr -e grt
+    abbr -e gs
+    abbr -e gss
+    abbr -e gsb
+    abbr -e gshow
+    abbr -e gshowps
+    abbr -e gst
+    abbr -e gsta
+    abbr -e gstc
+    abbr -e gstd
+    abbr -e gstl
+    abbr -e gstp
+    abbr -e gstshow
+    abbr -e gstall
+    abbr -e gsts
+    abbr -e gsu
+    abbr -e gsw
+    abbr -e gswc
+    abbr -e gswm
+    abbr -e gswd
+    abbr -e gt
+    abbr -e gts
+    abbr -e gta
+    abbr -e gtas
+    abbr -e gwch
+    abbr -e gwt
+    abbr -e gwta
+    abbr -e gwtls
+    abbr -e gwtmv
+    abbr -e gwtrm
+    abbr -e gam
+    abbr -e gamc
+    abbr -e gams
+    abbr -e gama
+    abbr -e gamscp
 end
